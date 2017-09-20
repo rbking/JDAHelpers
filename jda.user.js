47a34265df97440ec3f56a2c08904e91900e847e
Robert King <robert.b.king@lowes.com>
2017-09-20 12:14:47 -0400

    Updated to v2.3.1

--- jda.user.js
+++ jda.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         JDAHelpers
 // @namespace    https://github.com/rbking/JDAHelpers.git
-// @version      2.3.0
+// @version      2.3.1
 // @description  Functions to streamline JDA inputs
 // @author       Robert King
 // @match        https://timssrvprod.myloweslife.com/tm/entry/*
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @require      https://cdn.rawgit.com/rbking/JDAHelpers/b510a597/jdaHelper.min.js
+// @require      https://cdn.rawgit.com/rbking/JDAHelpers/76c9fd76/jdaHelper.min.js
 // ==/UserScript==
 
 (function(jda) {
